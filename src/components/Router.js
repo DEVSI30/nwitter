42f3b8bb4b8097e6035a16dfdb0c0a2629a407fe
devsi30 <offset654@gmail.com>
2022-02-11 22:39:11 +0900

    9장 완료

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,6 +3,7 @@ import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Navigation from "components/Navigation";
 import Profile from "../routes/Profile";
+import {Fragment} from "react";
 
 const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
     // 구조 분해 할당
@@ -12,8 +13,33 @@ const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
             <Routes>
                 {isLoggedIn ? (
                     <>
-                        <Route path='/' element={<Home userObj={userObj}/>} />
-                        <Route path='/profile' element={<Profile userObj={userObj} refreshUser={refreshUser}/>} />
+                        <Route path='/'
+                               element={
+                                   <div style={{
+                                       maxWidth: 890,
+                                       width: "100%",
+                                       margin: "0 auto",
+                                       marginTop: 80,
+                                       display: "flex",
+                                       justifyContent: "center",
+                                   }}>
+                                       <Home userObj={userObj}/>
+                                   </div>
+                               }/>
+                        <Route path='/profile'
+                               element={
+                                   <div style={{
+                                       maxWidth: 890,
+                                       width: "100%",
+                                       margin: "0 auto",
+                                       marginTop: 80,
+                                       display: "flex",
+                                       justifyContent: "center",
+                                   }}>
+                                       <Profile userObj={userObj}
+                                                refreshUser={refreshUser}/>
+                                   </div>
+                               }/>
                     </>
                 ) : (
                     <Route path='/' element={<Auth/>} />
